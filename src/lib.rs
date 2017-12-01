2763fe9ac38b6ea3b4b5c41683f23240a1946f81
Edward Faulkner <ef@alum.mit.edu>
2017-12-01 12:35:12 -0500

    switched to str solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,61 @@
 #[cfg(test)]
 mod tests_1_1 {
-    use solve_1_1 as solve;
+    use solve_1_1::solve_1_1 as solve;
 
     #[test]
     fn first_example() {
-        let answer = solve(1122);
+        let answer = solve("1122");
         assert_eq!(3, answer);
     }
 
     #[test]
     fn second_example() {
-        let answer = solve(1111);
+        let answer = solve("1111");
         assert_eq!(4, answer);
     }
 
     #[test]
     fn third_example() {
-        let answer = solve(1234);
+        let answer = solve("1234");
         assert_eq!(0, answer);
     }
 
     #[test]
     fn fourth_example() {
-        let answer = solve(91212129);
+        let answer = solve("91212129");
         assert_eq!(9, answer);
     }
 
 }
 
-use std::option::Option;
+pub mod solve_1_1 {
+    use std::option::Option;
 
-fn maybe_accum(accum: &mut i64, a: i64, b: i64) {
-    if a == b {
-        *accum += a;
+    pub fn solve_1_1(x: &str) -> u32 {
+        let mut previous : Option<char> = None;
+        let mut accum : u32 = 0;
+        let mut first : Option<char> = None;
+        for digit in x.chars() {
+            if let Some(value) = previous {
+                if value == digit {
+                    accum += digit.to_digit(10).unwrap();
+                }
+            } else {
+                first = Some(digit);
+            }
+            previous = Some(digit);
+        }
+
+        if let Some(first_value) = first {
+            if let Some(previous_value) = previous {
+                if first_value == previous_value {
+                    accum += previous_value.to_digit(10).unwrap();
+                }
+            }
+        }
+
+        accum
     }
 }
 
-pub fn solve_1_1(x: i64) -> i64 {
-    let mut remaining = x;
-    let mut previous : Option<i64> = None;
-    let mut accum : i64 = 0;
-    while remaining > 0 {
-        let digit = remaining % 10;
-        if let Some(value) = previous {
-            maybe_accum(&mut accum, value, digit);
-        }
-        remaining = remaining / 10;
-        previous = Some(digit);
-        if remaining == 0 {
-            maybe_accum(&mut accum, digit, x % 10);
-        }
-    }
 
-    accum
-}
